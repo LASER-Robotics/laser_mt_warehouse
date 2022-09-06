6f4b64a4a7f81a0c3a743197026306936d735c29
Augusto Vinicius <augustoviniciud@gmail.com>
2022-09-06 16:07:53 -0300

    Atualizações no arquivo da esp

--- Mambo/src/camera_drone/camera_processing.py
+++ Mambo/src/camera_drone/camera_processing.py
@@ -1,19 +1,20 @@
 import cv2, time, webbrowser
 
-def getImgBarCode(ipAddress):
-    webbrowser.open(ipAddress)
+#def getImgBarCode(ipAddress):
+#    webbrowser.open(ipAddress)
 
-ipAddress = input("Ip address of camera:\n")
+#ipAddress = input("Ip address of camera:\n")
 
-while True:
-    getImgBarCode(ipAddress)
-    imgPath = input("Caminho da imagem do código de barras\n")
+#while True:
+#getImgBarCode(ipAddress)
+#imgPath = input("Caminho da imagem do código de barras\n")
 
-    barCodeDetector = cv2.barcode_BarcodeDetector()
-    img = cv2.imread(imgPath)
+barCodeDetector = cv2.barcode_BarcodeDetector()
+img = cv2.imread("/home/laser/Desktop/Mambo-Turtle-Warehouse/Mambo/src/camera_drone/Imgs_tests/como-os-codigos-de-barras-funcionam.mp4")
 
-    ok, decodedInfo, decodedType, corners = barCodeDetector.detectAndDecode(img)
+ok, decodedInfo, decodedType, corners = barCodeDetector.detectAndDecode(img)
 
-    print(f"Decoded barcode information: {decodedInfo[0]}")
+print(ok)
+print(f"Decoded barcode information: {decodedInfo}")
 
-    time.sleep(2)
+#time.sleep(2)
