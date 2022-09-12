ea9ee0cd216f82773a17d326a8e9b2c8e7dc7349
Augusto Vinicius <augustoviniciud@gmail.com>
2022-09-12 17:59:20 -0300

    Código da esp terminado, falta realizar teste e configurar o drive

--- Mambo/src/camera_drone/camera_drone.ino
+++ Mambo/src/camera_drone/camera_drone.ino
@@ -1,59 +1,9 @@
 #include "esp_camera.h"
 #include <WiFi.h>
 
-//
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-//
-
-// Select camera model
-// #define CAMERA_MODEL_WROVER_KIT
-// #define CAMERA_MODEL_M5STACK_PSRAM
 #define CAMERA_MODEL_AI_THINKER
-// #define CAMERA_MODEL_TTGO
 
-const char* ssid = "LAB_ROBOTICA";
-const char* password = "UAV1X500";
-
-#if defined(CAMERA_MODEL_WROVER_KIT)
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    21
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
-
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      19
-#define Y4_GPIO_NUM      18
-#define Y3_GPIO_NUM       5
-#define Y2_GPIO_NUM       4
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
-
-#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    15
-#define XCLK_GPIO_NUM     27
-#define SIOD_GPIO_NUM     25
-#define SIOC_GPIO_NUM     23
-
-#define Y9_GPIO_NUM       19
-#define Y8_GPIO_NUM       36
-#define Y7_GPIO_NUM       18
-#define Y6_GPIO_NUM       39
-#define Y5_GPIO_NUM        5
-#define Y4_GPIO_NUM       34
-#define Y3_GPIO_NUM       35
-#define Y2_GPIO_NUM       32
-#define VSYNC_GPIO_NUM    22
-#define HREF_GPIO_NUM     26
-#define PCLK_GPIO_NUM     21
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
+#if defined(CAMERA_MODEL_AI_THINKER)
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
@@ -72,29 +22,32 @@ const char* password = "UAV1X500";
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-#elif defined(CAMERA_MODEL_TTGO)
-#define PWDN_GPIO_NUM     0
-#define RESET_GPIO_NUM    15
-#define XCLK_GPIO_NUM     27
-#define SIOD_GPIO_NUM     25
-#define SIOC_GPIO_NUM     23
+#define CAPTURE_TIME 3000;
+#define TIME_LIMIT 2000;
 
-#define Y9_GPIO_NUM       19
-#define Y8_GPIO_NUM       36
-#define Y7_GPIO_NUM       18
-#define Y6_GPIO_NUM       39
-#define Y5_GPIO_NUM       5
-#define Y4_GPIO_NUM       34
-#define Y3_GPIO_NUM       35
-#define Y2_GPIO_NUM       17
-#define VSYNC_GPIO_NUM    22
-#define HREF_GPIO_NUM     26
-#define PCLK_GPIO_NUM     21
-#else
-#error "Camera model not selected"
-#endif
+long int timeInitial = millis();
+String ssid = "LAB_ROBOTICA";
+String password = "UAV1X500";
+String host = "script.google.com";
+String meuScript = "/macros/s/XXX/exec";
 
-void startCameraServer();
+
+void wifiConect(String ssid, String password) {
+  WiFi.begin(ssid, password);
+  int tempoConexao = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    println(".");
+    delay(300);
+
+    if((tempoConexao + TIME_LIMIT) < millis()) {
+      println("Time limit for conect wifi, restarting ESP");
+      ESP.restart();
+      return;
+    }
+  }
+  
+  println("Wifi connected with sucessfuly");
+}
 
 void setup() {
   Serial.begin(115200);
@@ -121,52 +74,115 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
+  config.jpeg_quality = 17;
+  config.frame_size = FRAMESIZE_VGA;
+  config.fb_count = 1;
 
-  // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
 
-  //drop down frame size for higher initial frame rate
-  sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_QVGA);
-
-  /*if(ACCESS_POINT)
-  {
-    WiFi.softAP(ssid, password);
-  }
-  else
-  {*/
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  //}
-
-  startCameraServer();
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-}//setup
+  wifiConect(ssid, password);
+}
 
 void loop() {
+  if((timeInitial + CAPTURE_TIME) < millis()) {
+    capImg();
+    timeInitial = millis();
+  }
+}
 
+void capImg() {
+  Serial.println("Connecting at " + host + ".");
+
+  WiFiClientSecure client;
+
+  if(client.connect(host, 443)) {
+    Serial.println("Connected with succesfuly.");
+
+    camera_fb_t *fb = nullptr;
+
+    fb = esp_camera_fb_get();
+
+    if(!fb) {
+      Serial.println("Image capture failed.");
+      delay(1000);
+      ESP.restart();
+      return;
+    }
+
+    char *input = (char*)fb->buf;
+    char output[base64_enc_len(3)];
+    String imageFile = "";
+
+    for(int i = 0; i < fb->len; i++) {
+      base64_encode(output, (input++), 3)/
+      if(i % 3 == 0) {
+        imageFile += urlencode(String(output));
+      } 
+    }
+
+    String data = nameFile + mimeType + Img;
+    esp_camera_fb_return(fb);
+
+    for(int index = 0; index < imageFile.length(); index += 1000) {
+      client.print(imageFile.sbstring(index, (index + 1000)));
+    }
+
+    Serial.println("Sending img for cloud, waiting answer.");
+    long int tempoConexao = millis();
+
+    while(!client.available()) {
+      Serial.println('.');
+      if((tempoConexao + TIME_LIMIT) < millis()) {
+        Serial.println("\nDon't received anser, send failed.\n");
+      }
+    }
+
+    while(client.available()) {
+      Serial.println(char(client.read()));
+    }
+  } else {
+    Serial.println("Connection at " + host + "failed.");
+  }
+  
+  client.stop();
+}
+
+String ulrEncode(String str) {
+  String encodedString = "";
+  char c;
+  char code0, code1, code2;
+
+  for(int i = 0; i < str.length(); i++) {
+      c = str.charAt(i);
+
+      if(c == ' ' || isalnum(c)) {
+        encodedString += c;
+      } else {
+        code1 = (c & 0xf) + '0';
+        
+        if((c & 0xf) > 9) {
+          code1 = (c & 0xf) - 10 + 'A';
+        }
+
+        c = (c >> 4) & 0xf;
+        code0 = c + '0';
+
+        if(c > 9) {
+          code0 = c - 10 'A';
+        }
+
+        code2 = '\0';
+        encodedString += '%';
+        encodedString += code0;
+        encodedString += code1;
+      }
+
+      yield();
+  }
+
+  return encodedString;
 }
