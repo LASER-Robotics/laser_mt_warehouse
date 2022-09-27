b72b36673cca6ee54d069fa79906acdbf853b318
JovemPedr0 <plvm0220@gmail.com>
2022-09-27 13:43:06 -0300

    .yaml route

--- Turtlebot2/src/maps/dilatation/mapping copy.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
-
-grayImage = cv.imread('/home/pedrolucas/pibiclaser/maps/dilatation/maze.png', 0) ## Open image and transformation to gray scale
-ret, auxMap = cv.threshold(grayImage, 0, 255, cv.THRESH_BINARY_INV) ## binary
-binaryMap = auxMap; 
