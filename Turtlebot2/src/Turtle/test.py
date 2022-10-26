91a31e3e0305c8cf2ae2dd56cc616adcc84af388
JovemPedr0 <plvm0220@gmail.com>
2022-10-26 17:03:50 -0300

    Almost² autonoumus driving

---
+++ Turtlebot2/src/Turtle/test.py
@@ -0,0 +1,12 @@
+from Turtle import Turtle
+import rospy
+
+rospy.init_node("follow_route", anonymous = False)
+
+test = Turtle()
+
+test.go_to_shelf();
+test.go_to_shelf();
+test.go_to_shelf();
+test.go_to_shelf();
+
