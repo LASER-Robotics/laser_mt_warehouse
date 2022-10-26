5afb9ad1c78b3c72d49bd65d43439e222240ff16
Augusto Vinicius <augustoviniciud@gmail.com>
2022-10-26 18:18:16 -0300

    atualizado pontos do mapa no yaml da rota

--- Turtlebot2/src/Turtle/test.py
+++ Turtlebot2/src/Turtle/test.py
@@ -5,8 +5,8 @@ rospy.init_node("follow_route", anonymous = False)
 
 test = Turtle()
 
-test.go_to_shelf();
-test.go_to_shelf();
-test.go_to_shelf();
-test.go_to_shelf();
+test.go_to_shelf()
+test.go_to_shelf()
+test.go_to_shelf()
+test.go_to_shelf()
 
